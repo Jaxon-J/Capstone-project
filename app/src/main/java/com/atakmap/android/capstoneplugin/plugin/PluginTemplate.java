6519c5737fa61077878499dc61d60aa92d3f7608
millerjw2000 <millerjw2000@gmail.com>
2025-03-04 22:06:48 -0500

    added basic ui features

--- app/src/main/java/com/atakmap/android/capstoneplugin/plugin/PluginTemplate.java
+++ app/src/main/java/com/atakmap/android/capstoneplugin/plugin/PluginTemplate.java
@@ -2,6 +2,8 @@
 package com.atakmap.android.capstoneplugin.plugin;
 
 import android.content.Context;
+import android.view.View;
+import android.widget.Button;
 
 import com.atak.plugins.impl.PluginContextProvider;
 import com.atak.plugins.impl.PluginLayoutInflater;
@@ -22,6 +24,8 @@ public class PluginTemplate implements IPlugin {
     IHostUIService uiService;
     ToolbarItem toolbarItem;
     Pane templatePane;
+    Pane secondaryPane;
+    Pane tertiaryPane;
 
     public PluginTemplate(IServiceController serviceController) {
         this.serviceController = serviceController;
@@ -78,8 +82,24 @@ public class PluginTemplate implements IPlugin {
             // In this case, using it is not necessary - but I am putting it here to remind
             // developers to look at this Inflator
 
-            templatePane = new PaneBuilder(PluginLayoutInflater.inflate(pluginContext,
-                    R.layout.main_layout, null))
+            View rootView = PluginLayoutInflater.inflate(pluginContext, R.layout.main_layout, null);
+
+            Button devicesButton = rootView.findViewById(R.id.devicesButton1);
+            Button sensorsButton = rootView.findViewById(R.id.sensorsButton1);
+
+            devicesButton.setOnClickListener(v -> {
+                uiService.closePane(templatePane);
+                uiService.showPane(secondaryPane,null);
+
+            });
+
+            sensorsButton.setOnClickListener(v -> {
+                uiService.closePane(templatePane);
+                uiService.showPane(tertiaryPane,null);
+
+            });
+
+            templatePane = new PaneBuilder(rootView)
                     // relative location is set to default; pane will switch location dependent on
                     // current orientation of device screen
                     .setMetaValue(Pane.RELATIVE_LOCATION, Pane.Location.Default)
@@ -88,7 +108,73 @@ public class PluginTemplate implements IPlugin {
                     // pane will take up 50% of screen height in portrait mode
                     .setMetaValue(Pane.PREFERRED_HEIGHT_RATIO, 0.5D)
                     .build();
+
+
         }
+
+        if (secondaryPane == null) {
+
+            View rootView = PluginLayoutInflater.inflate(pluginContext, R.layout.secondary_layout, null);
+
+            Button trackingButton = rootView.findViewById(R.id.trackingButton2);
+            Button sensorsButton = rootView.findViewById(R.id.sensorsButton2);
+
+            trackingButton.setOnClickListener(v -> {
+                uiService.closePane(secondaryPane);
+                uiService.showPane(templatePane,null);
+
+            });
+
+            sensorsButton.setOnClickListener(v -> {
+                uiService.closePane(secondaryPane);
+                uiService.showPane(tertiaryPane,null);
+
+            });
+
+            secondaryPane = new PaneBuilder(rootView)
+                    // relative location is set to default; pane will switch location dependent on
+                    // current orientation of device screen
+                    .setMetaValue(Pane.RELATIVE_LOCATION, Pane.Location.Default)
+                    // pane will take up 50% of screen width in landscape mode
+                    .setMetaValue(Pane.PREFERRED_WIDTH_RATIO, 0.5D)
+                    // pane will take up 50% of screen height in portrait mode
+                    .setMetaValue(Pane.PREFERRED_HEIGHT_RATIO, 0.5D)
+                    .build();
+
+        }
+
+        if (tertiaryPane == null) {
+
+            View rootView = PluginLayoutInflater.inflate(pluginContext, R.layout.tertiary_layout, null);
+
+            Button trackingButton = rootView.findViewById(R.id.trackingButton3);
+            Button devicesButton = rootView.findViewById(R.id.devicesButton3);
+
+            trackingButton.setOnClickListener(v -> {
+                uiService.closePane(tertiaryPane);
+                uiService.showPane(templatePane,null);
+
+            });
+
+            devicesButton.setOnClickListener(v -> {
+                uiService.closePane(tertiaryPane);
+                uiService.showPane(secondaryPane,null);
+
+            });
+
+            tertiaryPane = new PaneBuilder(rootView)
+                    // relative location is set to default; pane will switch location dependent on
+                    // current orientation of device screen
+                    .setMetaValue(Pane.RELATIVE_LOCATION, Pane.Location.Default)
+                    // pane will take up 50% of screen width in landscape mode
+                    .setMetaValue(Pane.PREFERRED_WIDTH_RATIO, 0.5D)
+                    // pane will take up 50% of screen height in portrait mode
+                    .setMetaValue(Pane.PREFERRED_HEIGHT_RATIO, 0.5D)
+                    .build();
+
+        }
+        ;
+
 
         // if the plugin pane is not visible, show it!
         if(!uiService.isPaneVisible(templatePane)) {
