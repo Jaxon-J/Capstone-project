9cf29bccddb85a9bf199ef26061b0f81c5fface1
Aaron <2858172+lith-x@users.noreply.github.com>
2025-03-08 17:56:53 -0600

    more error logging

--- app/src/main/java/com/atakmap/android/trackingplugin/BluetoothReceiver.java
+++ app/src/main/java/com/atakmap/android/trackingplugin/BluetoothReceiver.java
@@ -17,7 +17,7 @@ import android.util.Log;
 
 public class BluetoothReceiver extends BroadcastReceiver {
     private static final String TAG = Constants.createTag(BluetoothReceiver.class);
-    private final BluetoothLeScanner scanner;
+    private BluetoothLeScanner scanner;
     private final ScanCallback scanCallback = new ScanCallback() {
         @SuppressLint("MissingPermission")
         @Override
@@ -28,24 +28,62 @@ public class BluetoothReceiver extends BroadcastReceiver {
             // TODO: device info is here. need to pass into somewhere.
             //  probably class variable passed in via constructor
         }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            switch (errorCode) {
+                case ScanCallback.SCAN_FAILED_SCANNING_TOO_FREQUENTLY:
+                    Log.e(TAG, "SCAN FAIL: Scanning too frequently...");
+                    break;
+                case ScanCallback.SCAN_FAILED_ALREADY_STARTED:
+                    Log.e(TAG, "SCAN FAIL: Another scan is already taking place.");
+                    break;
+                case ScanCallback.SCAN_FAILED_APPLICATION_REGISTRATION_FAILED:
+                    Log.e(TAG, "SCAN FAIL: Application registration failed.");
+                    break;
+                case ScanCallback.SCAN_FAILED_FEATURE_UNSUPPORTED:
+                    Log.e(TAG, "SCAN FAIL: Bluetooth LE scanner is not supported on this device.");
+                    break;
+                case ScanCallback.SCAN_FAILED_INTERNAL_ERROR:
+                    Log.e(TAG, "SCAN FAIL: Something just straight up broke.");
+                    break;
+                case ScanCallback.SCAN_FAILED_OUT_OF_HARDWARE_RESOURCES:
+                    Log.e(TAG, "SCAN FAIL: Hardware resources unavailable for Bluetooth LE scan.");
+                    break;
+            }
+        }
     };
 
     public BluetoothReceiver(Context context) {
-        // TODO: add null checks in here, log on error. set private variable
-        //  canScan, update UI behavior based what the getter of canScan returns.
-        //  maybe have a refresh button that will do the check again and updates canScan.
+        // TODO: add null checks in here, log on error.
         BluetoothManager manager =
                 (BluetoothManager) context.getSystemService(Context.BLUETOOTH_SERVICE);
+        if (manager == null) {
+            Log.e(TAG, "Could not get bluetooth manager. Bluetooth may not be supported on this device.");
+            return;
+        }
         BluetoothAdapter btAdapter = manager.getAdapter();
+        if (btAdapter == null) {
+            Log.e(TAG, "Could not get bluetooth adapter for some reason.");
+            return;
+        }
         this.scanner = btAdapter.getBluetoothLeScanner();
     }
 
     @SuppressLint("MissingPermission")
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d(TAG, "Got intent");
         String action = intent.getAction();
-        if (action == null || !PermissionsActivity.hasAllBtPermissions(context)) return;
+        if (action == null) {
+            Log.w(TAG, "Action was null. Returning...");
+            return;
+        } else if (!PermissionsActivity.hasAllBtPermissions(context)) {
+            Log.e(TAG, "Returning..."); // hasAllBtPermissions does logging
+            return;
+        } else if (scanner == null) {
+            Log.w(TAG, "Scanner was not initialized properly. Returning...");
+            return;
+        }
 
         switch (action) {
             case ACTIONS.START_SCAN:
