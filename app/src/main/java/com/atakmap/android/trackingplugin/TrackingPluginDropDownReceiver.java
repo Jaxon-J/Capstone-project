0fbda11a1488c8f02acee9b94492e1a833c9d895
Aaron <2858172+lith-x@users.noreply.github.com>
2025-03-11 18:44:16 -0500

    no longer crashes :)

--- app/src/main/java/com/atakmap/android/trackingplugin/TrackingPluginDropDownReceiver.java
+++ app/src/main/java/com/atakmap/android/trackingplugin/TrackingPluginDropDownReceiver.java
@@ -33,7 +33,7 @@ public class TrackingPluginDropDownReceiver extends DropDownReceiver {
             new Pair<>("Devices", R.layout.devices_layout),
             new Pair<>("Sensors", R.layout.sensors_layout),
             new Pair<>("Debug", R.layout.debug_layout));
-    BluetoothReceiver btReceiver;
+    public static BluetoothReceiver btReceiver;
 
     protected TrackingPluginDropDownReceiver(final MapView mapView, final Context context) {
         super(mapView);
@@ -64,64 +64,9 @@ public class TrackingPluginDropDownReceiver extends DropDownReceiver {
                 (tab, position) -> tab.setText(tabInfo.get(position).first));
         mediator.attach();
 
+        btReceiver = new BluetoothReceiver(pluginContext);
 
         // debug marker placement
-        mainView.findViewById(R.id.debugPlacePinButton).setOnClickListener((View v) -> {
-            GeoPoint selfPoint = MapView.getMapView().getSelfMarker().getPoint();
-            GeoPoint trackedPoint = new GeoPoint(selfPoint.getLatitude(), // 0.0000035 = 10ft
-                    selfPoint.getLongitude(), selfPoint.getAltitude(),
-                    selfPoint.getAltitudeReference(), 11, 11);
-            PlacePointTool.MarkerCreator mc = new PlacePointTool.MarkerCreator(trackedPoint);
-            mc.setType("a-u-G");
-            mc.setCallsign("tracked");
-            Marker trackedMarker = mc.placePoint();
-        });
-
-        // debug bluetooth scanning
-        btReceiver = new BluetoothReceiver(pluginContext);
-        AtakBroadcast.DocumentedIntentFilter btIntentFilter = new AtakBroadcast.DocumentedIntentFilter();
-        btIntentFilter.addAction(BluetoothReceiver.ACTIONS.BLE_START_SCAN);
-        btIntentFilter.addAction(BluetoothReceiver.ACTIONS.BLE_STOP_SCAN);
-        btIntentFilter.addAction(BluetoothReceiver.ACTIONS.CLASSIC_START_DISCOVERY);
-        btIntentFilter.addAction(BluetoothReceiver.ACTIONS.CLASSIC_STOP_DISCOVERY);
-        btIntentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-        btIntentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-        btIntentFilter.addAction(BluetoothDevice.ACTION_FOUND);
-        AtakBroadcast.getInstance().registerReceiver(btReceiver, btIntentFilter);
-
-        mainView.findViewById(R.id.bleScanDebugButton)
-                .setOnClickListener(this::onBleScanDebugButtonClick);
-        mainView.findViewById(R.id.classicScanDebugButton)
-                .setOnClickListener(this::onClassicScanDebugButtonClick);
-    }
-
-    private void onBleScanDebugButtonClick(View v) {
-        Button b = (Button) v;
-        boolean isEnabled = b.getText().equals(pluginContext.getString(R.string.ble_scan_enabled));
-        if (isEnabled) {
-            Intent stopScanIntent = new Intent(BluetoothReceiver.ACTIONS.BLE_STOP_SCAN);
-            AtakBroadcast.getInstance().sendBroadcast(stopScanIntent);
-            b.setText(pluginContext.getString(R.string.ble_scan_disabled));
-            return;
-        }
-        Intent startScanIntent = new Intent(BluetoothReceiver.ACTIONS.BLE_START_SCAN);
-        AtakBroadcast.getInstance().sendBroadcast(startScanIntent);
-        b.setText(pluginContext.getString(R.string.ble_scan_enabled));
-    }
-
-    private void onClassicScanDebugButtonClick(View v) {
-        Button b = (Button) v;
-        boolean isEnabled = b.getText()
-                .equals(pluginContext.getString(R.string.classic_scan_enabled));
-        if (isEnabled) {
-            Intent stopScanIntent = new Intent(BluetoothReceiver.ACTIONS.CLASSIC_STOP_DISCOVERY);
-            AtakBroadcast.getInstance().sendBroadcast(stopScanIntent);
-            b.setText(pluginContext.getString(R.string.classic_scan_disabled));
-            return;
-        }
-        Intent startScanIntent = new Intent(BluetoothReceiver.ACTIONS.CLASSIC_START_DISCOVERY);
-        AtakBroadcast.getInstance().sendBroadcast(startScanIntent);
-        b.setText(pluginContext.getString(R.string.classic_scan_enabled));
     }
 
     @Override
@@ -151,46 +96,6 @@ public class TrackingPluginDropDownReceiver extends DropDownReceiver {
     public static final class ACTIONS {
         public static final String SHOW_PLUGIN = "com.atakmap.android.trackingplugin.SHOW_PLUGIN";
     }
-
-
-//    private class TabViewPagerAdapter extends FragmentStateAdapter {
-//
-//        TabData[] tabs = {
-//                new TabData(R.string.tracking_tab, R.layout.tracking_layout),
-//                new TabData(R.string.devices_tab, R.layout.devices_layout),
-//                new TabData(R.string.sensors_tab, R.layout.sensors_layout),
-//                new TabData(R.string.debug_tab, R.layout.debug_layout)
-//        };
-//
-//        public void something(Context context) {
-//
-//        }
-//        public TabViewPagerAdapter(@NonNull FragmentManager fragmentManager, @NonNull Lifecycle
-//        lifecycle) {
-//            super(fragmentManager, lifecycle);
-//        }
-//
-//
-//        @NonNull
-//        @Override
-//        public Fragment createFragment(int position) {
-//            return tabs[position].fragment;
-//        }
-//
-//        @Override
-//        public int getItemCount() {
-//            return tabs.length;
-//        }
-//    }
-//
-//    private final class TabData {
-//        public final String name;
-//        public final Fragment fragment;
-//        public TabData(int stringId, int layoutId) {
-//            this.name = pluginContext.getString(stringId);
-//            this.fragment = new Fragment(layoutId);
-//        }
-//    }
 
     // If this ever gets refactored out, it needs the pluginContext and tabInfo passed in as
     // parameters
--- app/src/main/java/com/atakmap/android/trackingplugin/ui/TabViewPagerAdapter.java
+++ app/src/main/java/com/atakmap/android/trackingplugin/ui/TabViewPagerAdapter.java
@@ -1,16 +1,28 @@
 package com.atakmap.android.trackingplugin.ui;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Context;
+import android.content.Intent;
 import android.util.Log;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.atakmap.android.ipc.AtakBroadcast;
+import com.atakmap.android.maps.MapView;
+import com.atakmap.android.maps.Marker;
+import com.atakmap.android.trackingplugin.BluetoothReceiver;
 import com.atakmap.android.trackingplugin.Constants;
+import com.atakmap.android.trackingplugin.TrackingPluginDropDownReceiver;
+import com.atakmap.android.trackingplugin.plugin.R;
+import com.atakmap.android.user.PlacePointTool;
+import com.atakmap.coremap.maps.coords.GeoPoint;
 
 import java.util.List;
 
@@ -36,9 +48,37 @@ public class TabViewPagerAdapter extends RecyclerView.Adapter<TabViewPagerAdapte
     @Override
     public void onBindViewHolder(@NonNull TabViewHolder holder, int position) {
         Log.d(TAG, "onBindViewHolder: " + tabInfo.get(position).first);
-        // switch (holder.tabName) {
+         switch (holder.tabName) {
+             case "Debug":
+                 holder.itemView.findViewById(R.id.debugPlacePinButton).setOnClickListener((View v) -> {
+                     GeoPoint selfPoint = MapView.getMapView().getSelfMarker().getPoint();
+                     GeoPoint trackedPoint = new GeoPoint(selfPoint.getLatitude(), // 0.0000035 = 10ft
+                             selfPoint.getLongitude(), selfPoint.getAltitude(),
+                             selfPoint.getAltitudeReference(), 11, 11);
+                     PlacePointTool.MarkerCreator mc = new PlacePointTool.MarkerCreator(trackedPoint);
+                     mc.setType("a-u-G");
+                     mc.setCallsign("tracked");
+                     Marker trackedMarker = mc.placePoint();
+                 });
+
+                 // debug bluetooth scanning
+                 AtakBroadcast.DocumentedIntentFilter btIntentFilter = new AtakBroadcast.DocumentedIntentFilter();
+                 btIntentFilter.addAction(BluetoothReceiver.ACTIONS.BLE_START_SCAN);
+                 btIntentFilter.addAction(BluetoothReceiver.ACTIONS.BLE_STOP_SCAN);
+                 btIntentFilter.addAction(BluetoothReceiver.ACTIONS.CLASSIC_START_DISCOVERY);
+                 btIntentFilter.addAction(BluetoothReceiver.ACTIONS.CLASSIC_STOP_DISCOVERY);
+                 btIntentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+                 btIntentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+                 btIntentFilter.addAction(BluetoothDevice.ACTION_FOUND);
+                 AtakBroadcast.getInstance().registerReceiver(TrackingPluginDropDownReceiver.btReceiver, btIntentFilter);
+
+
+                 holder.itemView.findViewById(R.id.bleScanDebugButton)
+                         .setOnClickListener(this::onBleScanDebugButtonClick);
+                 holder.itemView.findViewById(R.id.classicScanDebugButton)
+                         .setOnClickListener(this::onClassicScanDebugButtonClick);
         // update tabs here, case [TabName]: [behavior]; break;
-        // }
+         }
     }
 
     @Override
@@ -58,5 +98,34 @@ public class TabViewPagerAdapter extends RecyclerView.Adapter<TabViewPagerAdapte
             super(itemView);
             this.tabName = name;
         }
+    }
+
+    private void onBleScanDebugButtonClick(View v) {
+        Button b = (Button) v;
+        boolean isEnabled = b.getText().equals(context.getString(R.string.ble_scan_enabled));
+        if (isEnabled) {
+            Intent stopScanIntent = new Intent(BluetoothReceiver.ACTIONS.BLE_STOP_SCAN);
+            AtakBroadcast.getInstance().sendBroadcast(stopScanIntent);
+            b.setText(context.getString(R.string.ble_scan_disabled));
+            return;
+        }
+        Intent startScanIntent = new Intent(BluetoothReceiver.ACTIONS.BLE_START_SCAN);
+        AtakBroadcast.getInstance().sendBroadcast(startScanIntent);
+        b.setText(context.getString(R.string.ble_scan_enabled));
+    }
+
+    private void onClassicScanDebugButtonClick(View v) {
+        Button b = (Button) v;
+        boolean isEnabled = b.getText()
+                .equals(context.getString(R.string.classic_scan_enabled));
+        if (isEnabled) {
+            Intent stopScanIntent = new Intent(BluetoothReceiver.ACTIONS.CLASSIC_STOP_DISCOVERY);
+            AtakBroadcast.getInstance().sendBroadcast(stopScanIntent);
+            b.setText(context.getString(R.string.classic_scan_disabled));
+            return;
+        }
+        Intent startScanIntent = new Intent(BluetoothReceiver.ACTIONS.CLASSIC_START_DISCOVERY);
+        AtakBroadcast.getInstance().sendBroadcast(startScanIntent);
+        b.setText(context.getString(R.string.classic_scan_enabled));
     }
 }
