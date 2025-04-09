cae88998aac844249d28debad1485832b2864e9b
tfryda <tfryda@unomaha.edu>
2025-04-08 19:06:54 -0500

    Added and created JUnit tests

---
+++ app/src/test/java/com/atakmap/android/test/TrackingPluginComponentsExistAndAreCompatible.java
@@ -0,0 +1,27 @@
+///This is a simple JUnit test that makes sure our core classes have not changed there tags, ensuring that a push has not
+/// messed with the core classes and how they exist everywhere else
+
+package com.atakmap.android.test;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.atakmap.android.trackingplugin.Constants;
+import com.atakmap.android.trackingplugin.TrackingPluginDropDownReceiver;
+import com.atakmap.android.trackingplugin.TrackingPluginMapComponent;
+
+
+public class TrackingPluginComponentsExistAndAreCompatible {
+    @Test
+    public void testCreateTag() {
+        String expectedTag = "TrackPlug.TrackingPluginMapComponent";
+        String actualTag = Constants.createTag(TrackingPluginMapComponent.class);
+        assertEquals(expectedTag, actualTag);
+
+        String expectedDropDownTag = "TrackPlug.TrackingPluginDropDownReceiver";
+        String actualDropDownTag = Constants.createTag(TrackingPluginDropDownReceiver.class);
+        assertEquals(expectedDropDownTag, actualDropDownTag);
+    }
+}
+
