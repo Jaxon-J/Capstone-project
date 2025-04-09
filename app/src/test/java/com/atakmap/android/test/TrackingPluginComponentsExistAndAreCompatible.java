ba076ac9f322a02832ece6c0c9ff6d900aefef39
tfryda <tfryda@unomaha.edu>
2025-04-09 15:52:49 -0500

    Created a new JUnit test to make sure our core classes have not been tampered with.

---
+++ app/src/test/java/com/atakmap/android/test/TrackingPluginComponentsExistAndAreCompatible.java
@@ -0,0 +1,36 @@
+///This is a simple JUnit test that makes sure our core classes have not changed there tags, ensuring that a push has not
+/// messed with the core classes and how they exist everywhere else
+
+package com.atakmap.android.test;
+
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.atakmap.android.trackingplugin.BluetoothReceiver;
+import com.atakmap.android.trackingplugin.Constants;
+import com.atakmap.android.trackingplugin.TrackingPluginDropDownReceiver;
+import com.atakmap.android.trackingplugin.TrackingPluginMapComponent;
+
+
+public class TrackingPluginComponentsExistAndAreCompatible {
+    @Test
+    public void testCreateTag() {
+        String expectedTag = "TrackPlug.TrackingPluginMapComponent";
+        String actualTag = Constants.createTag(TrackingPluginMapComponent.class);
+        assertEquals(expectedTag, actualTag);
+
+        String expectedDropDownTag = "TrackPlug.TrackingPluginDropDownReceiver";
+        String actualDropDownTag = Constants.createTag(TrackingPluginDropDownReceiver.class);
+        assertEquals(expectedDropDownTag, actualDropDownTag);
+
+        String expectedConstantsTag = "TrackPlug.Constants";
+        String actualConstantsTag = Constants.createTag(Constants.class);
+        assertEquals(expectedConstantsTag, actualConstantsTag);
+
+        String expectedReceiverTag = "TrackPlug.BluetoothReceiver";
+        String actualReceiverTag = Constants.createTag(BluetoothReceiver.class);
+        assertEquals(expectedReceiverTag, actualReceiverTag);
+    }
+}
+
